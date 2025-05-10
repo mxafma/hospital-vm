e9df14d746d173c62631f5cc0b29268a91c3e6e3
YuriDuran <yu.duran@duocuc.cl>
2025-05-10 09:53:03 -0400

    Agregue controlladores

--- src/main/java/com/hospital_vm/cl/hospital_vm/controller/PacienteController.java
+++ src/main/java/com/hospital_vm/cl/hospital_vm/controller/PacienteController.java
@@ -5,12 +5,17 @@ import com.hospital_vm.cl.hospital_vm.service.PacienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
@@ -28,7 +33,36 @@ public class PacienteController {
 
         }
         return ResponseEntity.ok(pacientes);
-        
+
     }
     
+    @PostMapping
+    public ResponseEntity<Paciente> guardar(@RequestBody Paciente paciente) {
+        
+        Paciente productoMuevo = pacienteService.save(paciente);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productoMuevo);
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Paciente> buscar(@PathVariable Interger id) {
+
+        try{
+            Paciente paciente = pacienteService.findById(id);
+            return ResponseEntity.ok(paciente);
+        }catch (Exception e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> eliminar (PathVariable Long id){
+        try{
+            pacienteService.delete(id);
+            return ResponseEntity.noContent().build();
+
+        }catch (Exception e){
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
